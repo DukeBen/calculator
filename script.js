60a0507faee433bd6a922715512a3a5c48f8c801
Ben Goldfried <2goldenfried@gmail.com>
2022-07-31 17:58:40 -0400

    Finish calculator logic and logistics

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let display = '';
+let op = '';
+let num2 = 0;
+let opClicked = false;
+
+
+setDisplay('0')
+
+//Calculator functions
 function add(a,b){
     return a+b;
 }
@@ -16,20 +25,83 @@ function divide(a,b){
 
 function operate(operation, num1, num2){
     let abvOp = operation.toLowerCase();
+    let val = 0;
     if(abvOp == 'add')
     {
-        console.log(add(num1,num2));
+        val = add(num1,num2);
+        setDisplay(val);
+        return val;
     }
     else if(abvOp == 'subtract')
     {
-        console.log(subtract(num1,num2));
+        val = subtract(num1,num2);
+        setDisplay(val);
+        return val;
     }
     else if(abvOp == 'multiply')
     {
-        console.log(multiply(num1,num2));
+        val = multiply(num1,num2);
+        setDisplay(val);
+        return val;
     }
     else if(abvOp == 'divide')
     {
-        console.log(divide(num1,num2));
+        val = divide(num1,num2)
+        setDisplay(val);
+        return val;
     }
 }
+
+function clear(){
+    display = '';
+    setDisplay('0');
+    opClicked = false;
+    num2 = 0;
+}
+
+//Wiring buttons to JS
+function setDisplay(str){
+    const disp = document.querySelector('.display');
+    disp.textContent = str;
+}
+
+const btns = document.querySelectorAll('.ops');
+btns.forEach((btn) => btn.addEventListener('click', function(e){
+    let allClasses = e.target.className;
+    let classesArr = allClasses.split(' ');
+    let firstClass = classesArr[0];
+    if(isNaN(firstClass) == false && opClicked == false)
+    {
+        display += firstClass;
+        setDisplay(display);
+    }
+    else if(isNaN(firstClass) && opClicked == false)
+    {
+        opClicked = true;
+        op = firstClass;
+    }
+    else if(display != '' && opClicked && isNaN(firstClass) == false){
+        num2 += parseInt(firstClass);
+        console.log(num2);
+    }
+    else if(opClicked && firstClass == 'equals')
+    {
+        display = operate(op, parseInt(display), num2);
+        opClicked = false;
+        op = '';
+        num2 = 0;
+    }
+    else if(opClicked && isNaN(firstClass)){
+        console.log('operation of '+ op);
+        display = operate(op, parseInt(display), num2);
+        console.log('display ' + display);
+        op = firstClass;
+        console.log('updated operation '+ op);
+        num2 = 0;
+    }
+    }))
+
+
+const clr = document.querySelector('.clear')
+clr.addEventListener('click', clear);
+
